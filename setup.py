a9178e5b3fa2dbcc90cf06140131a1566df3381b
Jens Boss <>
2020-04-19 18:17:07 +0200

    clean up

--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ setup(
     name="dynprog",
     version="0.0.1",
     description="Solve dynamical programming problems for the optimization of hydro power plants.",
-    py_modules=["model","senario","dynprog"],
+    py_modules=["model", "senario", "dyn_prog"],
     package_dir={'':'src'}
 )
