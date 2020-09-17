11fc2ee7455c87e0cefbff4c1bfdf4cb63d1ad5e
Jens Boss <bossjens@gmail.com>
2020-09-17 19:42:12 +0200

    added sparse_diag to utils and made proper test

--- src/hydropt/utils.py
+++ src/hydropt/utils.py
@@ -1,9 +1,9 @@
 import torch
 
 def tensor_kron(t1: torch.tensor, t2: torch.tensor):
+    """Compute the Kronecker product between two tensors.
     """
-    Computes the Kronecker product between two tensors.
-    """
+    
     t1_height, t1_width = t1.size()
     t2_height, t2_width = t2.size()
     
@@ -19,3 +19,30 @@ def tensor_kron(t1: torch.tensor, t2: torch.tensor):
     )
 
     return expanded_t1 * tiled_t2
+
+
+def tensor_sparse_diags(diags, offsets, size):
+    """Construct sparse diagonal tensor of dtype float64.
+    """
+    
+    indices = []
+    values = []
+    
+    for diag, offset in zip(diags, offsets):
+        
+        j = torch.arange(start=max(0,offset),
+                        end=min(size[1], size[0]+offset),
+                        dtype=torch.int64)
+        
+        i = torch.arange(start=max(0,-offset),
+                        end=min(size[0], size[1]-offset),
+                        dtype=torch.int64)    
+        
+        indices.append(torch.stack((i,j)))
+        
+        values.append(torch.DoubleTensor([diag,]
+                                        ).repeat(i.size())) 
+    
+    return torch.sparse.FloatTensor(torch.cat(indices,1), 
+                                    torch.cat(values), 
+                                    size)
--- tests/test_sparse_diag.py
+++ tests/test_sparse_diag.py
@@ -1,43 +1,65 @@
 from hydropt.core import sparse
 import torch
+import numpy as np
+
+from hydropt.utils import tensor_sparse_diags
+
+class TestTensorSparseDiags():
+    def test_square_matrix(self):
+        offsets = [0, 1, -1]
+        diags = [0.1, 0.9, 0.8]
+        
+        shape = (4, 4)
+        
+        diag_scipy = sparse.diags(diags, offsets=offsets, shape=shape)
+        
+        diag_torch = tensor_sparse_diags(diags, offsets=offsets, size=shape)
+        
+        assert np.all(diag_scipy.todense() == diag_torch.to_dense().numpy())
+        
+    def test_wide_matrix(self):
+        offsets = [0, 3, -1]
+        diags = [0.1, 0.9, 0.8]
+        
+        shape = (4, 8)
+        
+        diag_scipy = sparse.diags(diags, offsets=offsets, shape=shape)
+        
+        diag_torch = tensor_sparse_diags(diags, offsets=offsets, size=shape)
+        
+        assert np.all(diag_scipy.todense() == diag_torch.to_dense().numpy())
+        
+    def test_long_matrix(self):
+        offsets = [0, 1, -2]
+        diags = [0.1, 0.9, 0.8]
+        
+        shape = (9, 7)
+        
+        diag_scipy = sparse.diags(diags, offsets=offsets, shape=shape)
+        
+        diag_torch = tensor_sparse_diags(diags, offsets=offsets, size=shape)
+        
+        assert np.all(diag_scipy.todense() == diag_torch.to_dense().numpy())
+        
+
+
 
 if __name__ == '__main__':
-    offsets = [0,1,-1]
-    diags = [0.1,0.9,0.8]
-    L = sparse.diags(diags, offsets=offsets, shape=(4,5))
-    print(L.todense())
-    L = sparse.diags(diags, offsets=offsets, shape=(5,4))
-    print(L.todense())
+    import timeit
     
-    offset = offsets[0]
-    
-    shape=(5,4)
-    
-    def sparse_diags(diags, offsets, size):
-                
-        indices = []
-        values = []
-        
-        for diag, offset in zip(diags, offsets):
+    offsets = [0, 1, -2]
+    diags = [0.1, 0.9, 0.8]        
+    shape = (9, 7)
             
-            j = torch.arange(start=max(0,offset),
-                            end=min(size[1], size[0]+offset),
-                            dtype=torch.int64)
-            
-            i = torch.arange(start=max(0,-offset),
-                            end=min(size[0], size[1]-offset),
-                            dtype=torch.int64)    
-            
-            indices.append(torch.stack((i,j)))
-            
-            values.append(torch.FloatTensor([diag,]).repeat(i.size())) 
-        
-        return torch.sparse.FloatTensor(
-            torch.cat(indices,1), 
-            torch.cat(values), 
-            size)
+    t = timeit.timeit(lambda: tensor_sparse_diags(diags, offsets=offsets, size=shape),
+                      number=100)
+    print('time tensor_sparse_diags (in sec):', t)
+    
+    t = timeit.timeit(lambda: sparse.diags(diags, offsets=offsets, shape=shape),
+                      number=100)
+    print('time scipy sparse.diags (in sec):', t)
     
     
-    print(sparse_diags(diags, offsets, shape).to_dense())
+    
     
     
