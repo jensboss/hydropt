4d3212a788de2993d06f8eaa7756f0b2e109815b
Jens Boss <bossjens@gmail.com>
2020-09-18 19:29:14 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -6,15 +6,31 @@ with open('README.md', 'r') as file:
 
 setup(
     name='hydropt',
-    version='0.0.1',
+    version='0.0.2',
+    author="Jens Boss",
+    author_email="bossjens@gmail.com",
     description='Solves dynamical programming problems for the optimization of hydro power plants.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url="https://github.com/yenzmike/hydropt",
     packages=['hydropt'],
     package_dir={'':'src'},
     include_package_data=True,
     package_data={'': ['data/*.csv']},
-    long_description=long_description,
-    long_description_content_type='text/markdown',
     classifiers=[
         "Programming Language :: Python :: 3",
-        ]
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+    install_requires=[
+        "pandas>=0.25.3",
+        "numpy>=1.17.4",
+        "scipy>=1.5.0"
+    ],
+    extras_require={
+        "dev": [
+            "pytest>=5.4.2",
+        ],
+    },
 )
